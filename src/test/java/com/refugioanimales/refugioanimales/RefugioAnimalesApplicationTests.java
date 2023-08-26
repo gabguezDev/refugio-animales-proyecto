b264416be01a0d9a99a46a5e43954861bbb272e8
Gabriel Dominguez <gabriel.dominguez.dev@gmail.com>
2023-08-26 16:23:56 -0300

    primer commit

---
+++ src/test/java/com/refugioanimales/refugioanimales/RefugioAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refugioanimales.refugioanimales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefugioAnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
